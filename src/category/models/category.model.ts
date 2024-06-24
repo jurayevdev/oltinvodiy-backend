6f7a3f27e8de2ea00a0c8b5a33db117b844f2d31
Davlat Jo'rayev <jurayevcoder@gmail.com>
2024-06-24 16:06:28 +0500

    change

--- src/category/models/category.model.ts
+++ src/category/models/category.model.ts
@@ -1,36 +1,36 @@
-import { Column, DataType, HasMany, Model, Table } from "sequelize-typescript";
-import { Menu } from "src/menu/models/menu.model";
+import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
+import { Menu } from 'src/menu/models/menu.model';
 
 interface CategoryAttr {
-    name_uzb: string;
-    name_rus: string;
-    name_eng: string;
+  name_uzb: string;
+  name_rus: string;
+  name_eng: string;
 }
 
-@Table({tableName: "category"})
+@Table({ tableName: 'category' })
 export class Category extends Model<Category, CategoryAttr> {
-    @Column({
-        type: DataType.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    })
-    id: number;
+  @Column({
+    type: DataType.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
 
-    @Column({
-        type: DataType.STRING,
-    })
-    name_uzb: string;
+  @Column({
+    type: DataType.STRING,
+  })
+  name_uzb: string;
 
-    @Column({
-        type: DataType.STRING,
-    })
-    name_rus: string;
+  @Column({
+    type: DataType.STRING,
+  })
+  name_rus: string;
 
-    @Column({
-        type: DataType.STRING,
-    })
-    name_eng: string;
+  @Column({
+    type: DataType.STRING,
+  })
+  name_eng: string;
 
-    @HasMany(() => Menu, { onDelete: 'CASCADE'})
-    menu: Menu;
+  @HasMany(() => Menu, { onDelete: 'CASCADE', hooks: true })
+  menu: Menu;
 }
